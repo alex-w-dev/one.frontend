4e7868c117f843f10081adb061b7020f9b73eb6f
Alexander-w-dev <yampi.ru@gmail.com>
2017-07-09 21:20:11 +0500

    client-list finished (verstka)

--- src/app/client-list/client-list.component.ts
+++ src/app/client-list/client-list.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IClientListClient {
+  img: string;
+  name: string;
+  lastSurvey: string; // date
+  lastAppointment: string; // date
+  access?: boolean;
+}
+
 @Component({
   selector: 'app-client-list',
   templateUrl: './client-list.component.html',
@@ -8,9 +16,21 @@ import { Component, OnInit } from '@angular/core';
 export class ClientListComponent implements OnInit {
   searchValue: string = '';
 
+  clients: IClientListClient[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    for (let i = 1; i < 11; i++) {
+      this.clients.push({
+        img: '../../public/img/client-list-avatear.png',
+        name: `Пручковская Маргарита Сергеевна`,
+        lastSurvey: `${i}.01.2017`,
+        lastAppointment: `${i}.04.2017`,
+        access: (i % 2) ? (!!(i % 3)) : undefined,
+      });
+
+    }
   }
 
   search() {
