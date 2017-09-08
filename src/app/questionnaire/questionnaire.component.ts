e00609014794062451e15d0d94db959c1fefcb6e
ArseniyDyupin <dyupinars@gmail.com>
2017-09-08 17:43:45 +0500

    Анкета пациента

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -28,6 +28,7 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
   active = true;
   isPatient = true;
   remove = false;
+  male: string = '';
 
   questionsForm: NgForm;
   @ViewChild('questionsForm') currentForm: NgForm;
@@ -44,6 +45,7 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.subscription = this.route.params.subscribe(params => this.id_measure = params['id_measure']);
     this.user = this.userService.getUser(this);
+    this.male = <string>this.user.male;
     if(this.user){
       if(this.user.isPartner()){
         this.id_measure = 800;
@@ -56,22 +58,24 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
       if (data.success && data.result && !!data.result.groups) {
         this.typeValues = 0;
         Object.keys(data.result.groups).forEach(questionKey => {
-          this.questions.push({
-            id_measure: data.result.groups[questionKey]['id_measure'],
-            id_parent: data.result.groups[questionKey]['id_parent'],
-            name: data.result.groups[questionKey]['name'],
-            typevalue: data.result.groups[questionKey]['typevalue'],
-            sort_order: data.result.groups[questionKey]['sort_order'],
-            age_low: data.result.groups[questionKey]['age_low'],
-            age_high: data.result.groups[questionKey]['age_high'],
-            male: data.result.groups[questionKey]['male'],
-            section: data.result.groups[questionKey]['section'],
-            answered: {
-              need: data.result.groups[questionKey]['answered']['need'],
-              answered: data.result.groups[questionKey]['answered']['answered'],
-              proc: data.result.groups[questionKey]['answered']['proc'],
-            }
-          });
+          if( data.result.groups[questionKey]['male'] === '2' || data.result.groups[questionKey]['male'] === this.male) {
+            this.questions.push({
+              id_measure: data.result.groups[questionKey]['id_measure'],
+              id_parent: data.result.groups[questionKey]['id_parent'],
+              name: data.result.groups[questionKey]['name'],
+              typevalue: data.result.groups[questionKey]['typevalue'],
+              sort_order: data.result.groups[questionKey]['sort_order'],
+              age_low: data.result.groups[questionKey]['age_low'],
+              age_high: data.result.groups[questionKey]['age_high'],
+              male: data.result.groups[questionKey]['male'],
+              section: data.result.groups[questionKey]['section'],
+              answered: {
+                need: data.result.groups[questionKey]['answered']['need'],
+                answered: data.result.groups[questionKey]['answered']['answered'],
+                proc: data.result.groups[questionKey]['answered']['proc'],
+              }
+            });
+          }
         });
       }
     });
@@ -85,27 +89,30 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
     this.questions = [];
     this.apiService.request('account/anketa', {'id_parent': this.id_measure}).then(data => {
 
-      console.log(this.questions);
+       console.log(data);
+       console.log(this.questions);
 
       if (data.success && data.result && !!data.result.groups) {
         this.typeValues = 0;
         Object.keys(data.result.groups).forEach(questionKey => {
-          this.questions.push({
-            id_measure: data.result.groups[questionKey]['id_measure'],
-            id_parent: data.result.groups[questionKey]['id_parent'],
-            name: data.result.groups[questionKey]['name'],
-            typevalue: data.result.groups[questionKey]['typevalue'],
-            sort_order: data.result.groups[questionKey]['sort_order'],
-            age_low: data.result.groups[questionKey]['age_low'],
-            age_high: data.result.groups[questionKey]['age_high'],
-            male: data.result.groups[questionKey]['male'],
-            section: data.result.groups[questionKey]['section'],
-            answered: {
-              need: data.result.groups[questionKey]['answered']['need'],
-              answered: data.result.groups[questionKey]['answered']['answered'],
-              proc: data.result.groups[questionKey]['answered']['proc'],
-            }
-          });
+          if( data.result.groups[questionKey]['male'] === '2' || data.result.groups[questionKey]['male'] === this.male) {
+            this.questions.push({
+              id_measure: data.result.groups[questionKey]['id_measure'],
+              id_parent: data.result.groups[questionKey]['id_parent'],
+              name: data.result.groups[questionKey]['name'],
+              typevalue: data.result.groups[questionKey]['typevalue'],
+              sort_order: data.result.groups[questionKey]['sort_order'],
+              age_low: data.result.groups[questionKey]['age_low'],
+              age_high: data.result.groups[questionKey]['age_high'],
+              male: data.result.groups[questionKey]['male'],
+              section: data.result.groups[questionKey]['section'],
+              answered: {
+                need: data.result.groups[questionKey]['answered']['need'],
+                answered: data.result.groups[questionKey]['answered']['answered'],
+                proc: data.result.groups[questionKey]['answered']['proc'],
+              }
+            });
+          }
         });
       }
       if (data.success && data.result && !!data.result.questions) {
@@ -123,50 +130,55 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
         });
         let i = 0;
         Object.keys(data.result.questions).forEach(questionKey => {
-          this.questions.push({
-            id_measure: data.result.questions[questionKey]['id_measure'],
-            id_parent: data.result.questions[questionKey]['id_parent'],
-            name: data.result.questions[questionKey]['name'],
-            typevalue: data.result.questions[questionKey]['typevalue'],
-            sort_order: data.result.questions[questionKey]['sort_order'],
-            age_low: data.result.questions[questionKey]['age_low'],
-            age_high: data.result.questions[questionKey]['age_high'],
-            male: data.result.questions[questionKey]['male'],
-            section: data.result.questions[questionKey]['section'],
-            array_key : i,
-            children: []
-          });
-
-          // делает сет чайлдов
-          this.setChildrenSection(this.questions[i].children, data.result.questions[questionKey]['children']);
-
-          if( data.result.questions[questionKey].values != null){
-            this.questions[i].values = [];
-            Object.keys(data.result.questions[questionKey].values).forEach(valueKey => {
-              this.questions[i].values.push({
-                id: data.result.questions[questionKey].values[valueKey]['id_valnominal'],
-                name: data.result.questions[questionKey].values[valueKey]['valnominal'],
-                value_group : data.result.questions[questionKey].values[valueKey]['id_measure'],
-                id_measure: data.result.questions[questionKey].values[valueKey]['id_measure'],
-                id_valnominal: data.result.questions[questionKey].values[valueKey]['id_valnominal'],
-                sort_order: data.result.questions[questionKey].values[valueKey]['sort_order'],
-                valnominal: data.result.questions[questionKey].values[valueKey]['valnominal'],
-              });
+          if( data.result.questions[questionKey]['male'] === '2' || data.result.questions[questionKey]['male'] === this.male) {
+            this.questions.push({
+              id_measure: data.result.questions[questionKey]['id_measure'],
+              id_parent: data.result.questions[questionKey]['id_parent'],
+              name: data.result.questions[questionKey]['name'],
+              typevalue: data.result.questions[questionKey]['typevalue'],
+              sort_order: data.result.questions[questionKey]['sort_order'],
+              age_low: data.result.questions[questionKey]['age_low'],
+              age_high: data.result.questions[questionKey]['age_high'],
+              male: data.result.questions[questionKey]['male'],
+              section: data.result.questions[questionKey]['section'],
+              array_key: i,
+              children: []
             });
+
+
+            // делает сет чайлдов
+            this.setChildrenSection(this.questions[i].children, data.result.questions[questionKey]['children']);
+
+            if (data.result.questions[questionKey].values != null) {
+              this.questions[i].values = [];
+              Object.keys(data.result.questions[questionKey].values).forEach(valueKey => {
+                this.questions[i].values.push({
+                  id: data.result.questions[questionKey].values[valueKey]['id_valnominal'],
+                  name: data.result.questions[questionKey].values[valueKey]['valnominal'],
+                  value_group: data.result.questions[questionKey].values[valueKey]['id_measure'],
+                  id_measure: data.result.questions[questionKey].values[valueKey]['id_measure'],
+                  id_valnominal: data.result.questions[questionKey].values[valueKey]['id_valnominal'],
+                  sort_order: data.result.questions[questionKey].values[valueKey]['sort_order'],
+                  valnominal: data.result.questions[questionKey].values[valueKey]['valnominal'],
+                });
+              });
+            }
+            let answerValue: number | string = '';
+            if (data.result.questions[questionKey]['value'] !== null) {
+              answerValue = data.result.questions[questionKey]['value']['value'];
+            }
+            if (data.result.questions[questionKey]['typevalue'] == 2 && data.result.questions[questionKey]['value'] == null) {
+              if (!!this.questions[i].values[0]) {
+                answerValue = this.questions[i].values[0].id;
+              }
+            }
+            this.answer.push({
+              type_value: data.result.questions[questionKey]['typevalue'],
+              value: answerValue,
+              measure_id: data.result.questions[questionKey]['id_measure'],
+            });
+            i++;
           }
-          let answerValue: number | string = '';
-          if( data.result.questions[questionKey]['value'] !== null){
-            answerValue = data.result.questions[questionKey]['value']['value'];
-          }
-          if( data.result.questions[questionKey]['typevalue'] == 2 && data.result.questions[questionKey]['value'] == null ){
-            answerValue = this.questions[i].values[0].id;
-          }
-          this.answer.push({
-            type_value: data.result.questions[questionKey]['typevalue'],
-            value : answerValue,
-            measure_id: data.result.questions[questionKey]['id_measure'],
-          });
-          i++;
         });
       }
     });
@@ -176,51 +188,52 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
     let j = 0;
     if(children.length > 0){
       Object.keys(children).forEach(childrenKey =>{
-         parent.push({
-           id_measure: children[childrenKey]['id_measure'],
-           id_parent: children[childrenKey]['id_parent'],
-           name: children[childrenKey]['name'],
-           typevalue: children[childrenKey]['typevalue'],
-           sort_order: children[childrenKey]['sort_order'],
-           age_low: children[childrenKey]['age_low'],
-           age_high: children[childrenKey]['age_high'],
-           male: children[childrenKey]['male'],
-           section: children[childrenKey]['section'],
-           children: [],
-           array_key : j,
-         });
-         this.setChildrenSection(parent[j].children, children[childrenKey]['children']);
+        if( children[childrenKey]['male'] === '2' || children[childrenKey]['male'] === this.male) {
+          parent.push({
+            id_measure: children[childrenKey]['id_measure'],
+            id_parent: children[childrenKey]['id_parent'],
+            name: children[childrenKey]['name'],
+            typevalue: children[childrenKey]['typevalue'],
+            sort_order: children[childrenKey]['sort_order'],
+            age_low: children[childrenKey]['age_low'],
+            age_high: children[childrenKey]['age_high'],
+            male: children[childrenKey]['male'],
+            section: children[childrenKey]['section'],
+            children: [],
+            array_key: j,
+          });
+          this.setChildrenSection(parent[j].children, children[childrenKey]['children']);
 
-         if(children[childrenKey].values != null) {
-           parent[j].values = [];
-           Object.keys(children[childrenKey].values).forEach(childrenValueKey => {
-             parent[j].values.push({
-               id: children[childrenKey].values[childrenValueKey]['id_valnominal'],
-               name: children[childrenKey].values[childrenValueKey]['valnominal'],
-               value_group : children[childrenKey].values[childrenValueKey]['id_measure'],
-               id_measure: children[childrenKey].values[childrenValueKey]['id_measure'],
-               id_valnominal: children[childrenKey].values[childrenValueKey]['id_valnominal'],
-               sort_order: children[childrenKey].values[childrenValueKey]['sort_order'],
-               valnominal: children[childrenKey].values[childrenValueKey]['valnominal'],
-             });
-           });
-         }
-        let answerValue: number | string = '';
-        if( children[childrenKey]['value'] !== null){
-          answerValue = children[childrenKey]['value']['value'];
-        }
-        if( children[childrenKey]['typevalue'] == 2 && children[childrenKey]['value'] == null ){
-          if(!!parent[j].values[0]) {
-            answerValue = parent[j].values[0].id;
-            console.log(parent[j].values[0]);
+          if (children[childrenKey].values != null) {
+            parent[j].values = [];
+            Object.keys(children[childrenKey].values).forEach(childrenValueKey => {
+              parent[j].values.push({
+                id: children[childrenKey].values[childrenValueKey]['id_valnominal'],
+                name: children[childrenKey].values[childrenValueKey]['valnominal'],
+                value_group: children[childrenKey].values[childrenValueKey]['id_measure'],
+                id_measure: children[childrenKey].values[childrenValueKey]['id_measure'],
+                id_valnominal: children[childrenKey].values[childrenValueKey]['id_valnominal'],
+                sort_order: children[childrenKey].values[childrenValueKey]['sort_order'],
+                valnominal: children[childrenKey].values[childrenValueKey]['valnominal'],
+              });
+            });
           }
+          let answerValue: number | string = '';
+          if (children[childrenKey]['value'] !== null) {
+            answerValue = children[childrenKey]['value']['value'];
+          }
+          if (children[childrenKey]['typevalue'] == 2 && children[childrenKey]['value'] == null) {
+            if (!!parent[j].values[0]) {
+              // answerValue = parent[j].values[0].id;
+            }
+          }
+          this.answer.push({
+            type_value: children[childrenKey]['typevalue'],
+            value: answerValue,
+            measure_id: children[childrenKey]['id_measure'],
+          });
+          j++;
         }
-        this.answer.push({
-          type_value: children[childrenKey]['typevalue'],
-          value : answerValue,
-          measure_id: children[childrenKey]['id_measure'],
-        });
-         j++;
        });
     }
   }
